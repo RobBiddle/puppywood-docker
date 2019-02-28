89f3990bdbb7f8f929ec2a58e46bede07d9805f7
Robert Biddle <RobBiddle@users.noreply.github.com>
2019-02-28 10:51:38 -0500

    Using Ubuntu 18.04LTS install instructions from here: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,24 @@
 sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 sudo apt install git -y
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh get-docker.sh
 sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
-sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo usermod -aG docker ${USER}
 sudo systemctl enable docker
 cd ~
 git clone https://github.com/RobBiddle/puppywood-docker.git
 mkdir -p /opt/webrtc-streamer
 sudo cp ~/puppywood-docker/config.json /opt/webrtc-streamer/config.json
 mkdir -p /opt/traefik
-sudo touch /opt/traefik/acme.json && chmod 600 /opt/traefik/acme.json
+sudo touch /opt/traefik/acme.json && sudo chmod 600 /opt/traefik/acme.json
 sudo cp ~/puppywood-docker/traefik.toml /opt/traefik/traefik.toml
 chmod +x ~/puppywood-docker/launch.sh
-sudo ~/puppywood-docker/launch.sh
+docker swarm init
+docker network create web
+. ~/puppywood-docker/launch.sh
--- launch.sh
+++ launch.sh
@@ -1,5 +1,1 @@
-docker swarm init
-docker network create web
 docker stack deploy -c ~/puppywood-docker/stack.yml cameras
-
-
