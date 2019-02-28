bdb94be6056e33856983ea561333dfb3b341488d
Robert Biddle <RobBiddle@users.noreply.github.com>
2019-02-28 12:42:47 -0500

    test

--- launch.sh
+++ launch.sh
@@ -2,4 +2,7 @@ sudo rm -rf ~/puppywood-docker
 git clone https://github.com/RobBiddle/puppywood-docker.git
 chmod +x ~/puppywood-docker/bootstrap.sh
 chmod +x ~/puppywood-docker/launch.sh
+docker stack rm cameras
+docker network rm web
+docker network create web --scope swarm
 docker stack deploy -c ~/puppywood-docker/stack.yml cameras
