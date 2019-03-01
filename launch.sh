3becc43af5c10c756f81b332d4a3e005381aeccd
Robert Biddle <RobBiddle@users.noreply.github.com>
2019-03-01 15:59:59 -0500

    Using mpeg-proxy for Montgomery cams.  Serving via nginx

--- launch.sh
+++ launch.sh
@@ -21,6 +21,7 @@ sudo cp ~/puppywood-docker/config.json /opt/webrtc-streamer/config.json
 sudo cp ~/puppywood-docker/config-beechmont.json /opt/webrtc-streamer/config-beechmont.json
 sudo cp ~/puppywood-docker/config-montgomery.json /opt/webrtc-streamer/config-montgomery.json
 sudo cp ~/puppywood-docker/index.html /opt/nginx/index.html
+sudo cp ~/puppywood-docker/montgomery-cams.html /opt/nginx/montgomery-cams.html
 sleep 1
 docker network create web --scope swarm --driver overlay
 sleep 3
